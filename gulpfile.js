a369d9acd21908b560b2cd44bc2a26a1af391142
Thomas Dustin Harrell <doostinharrell@gmail.com>
2015-10-11 20:07:28 -0400

    updated for sharing

--- gulpfile.js
+++ gulpfile.js
@@ -9,7 +9,6 @@ var gulp          = require('gulp'),
     nodesass      = require('node-sass'),
     sass          = require('gulp-sass'),
     globbing      = require('gulp-css-globbing'),
-    autoprefixer  = require('gulp-autoprefixer'),
     minifycss     = require('gulp-minify-css'),
     sourcemaps    = require('gulp-sourcemaps'),
     bowerfiles    = require('main-bower-files'),
@@ -22,9 +21,6 @@ gulp.task('bower-build', function() {
   gulp.src(bowerfiles({ // Get main js files
     includeDev:'true',
     overrides: {
-//      jquery: {
-//        main: true // Ignore bower jQuery in favore of Drupal
-//      },
       "modernizr-min": {
         main: "*.js" // Manual main file definition for modernizr
       }
@@ -42,14 +38,6 @@ gulp.task('bower-build', function() {
     .pipe(gulp.dest('dev/sass/bower'))
 })
 
-// Pre-JS Task
-gulp.task('pre-js', function() {
-  gulp.src('dev/js/bower/*.js')
-    .pipe(uglify())
-    .pipe(concat({ path: 'apps.js', stat: {mode: 0666} }))
-    .pipe(gulp.dest('dev/js'))
-})
-
 // JS Task
 gulp.task('js', function() {
   gulp.src('dev/js/*.js')
@@ -60,40 +48,19 @@ gulp.task('js', function() {
 
 // Sass Task
 gulp.task('sass', function() {
-  gulp.src('dev/sass/dustinharrell.styles.scss')
+  gulp.src('dev/sass/styles.scss')
     .pipe(sourcemaps.init())
     .pipe(globbing({ extensions: ['.scss'] }))
     .pipe(sass())
-    .pipe(autoprefixer())
     .pipe(minifycss())
     .pipe(sourcemaps.write('../maps/css'))
     .pipe(gulp.dest('build/css'))
-
-    // Reload browser
-    .pipe(reload({stream: true}))
-})
-
-// Browser Sync
-gulp.task('browser-sync', function() {
-  browsersync({
-    server: {
-        baseDir: './'
-    },
-    port: 3000,
-    ui: {
-      port: 3001,
-      weinre: {
-        port: 3002
-      }
-    }
-  })
 })
 
 // Watch for changes
 gulp.task('watch', function(){
-  gulp.watch('dev/**/*', ['pre-js', 'js', 'sass'])
-  gulp.watch('*.html').on('change', browsersync.reload);
+  gulp.watch('dev/**/*', ['js', 'sass'])
 })
 
 // Run run browser-sync and watch for changes
-gulp.task('default', ['browser-sync', 'watch'])
+gulp.task('default', ['watch'])
